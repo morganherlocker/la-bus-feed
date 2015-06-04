507ddb81f56595081a4d9aec635ad27e8d8faa74
morganherlocker <morgan.herlocker@gmail.com>
2015-06-04 12:23:04 -0400

    swallow errors

--- index.js
+++ index.js
@@ -7,35 +7,39 @@ var currentFile = ''
 
 setInterval(function(){
   request('http://api.metro.net/agencies/lametro/vehicles/', function(err, res, body){
-    var data = JSON.parse(body).items
-    var time = new Date()
+    try{
+      var data = JSON.parse(body).items
+      var time = new Date()
 
-    var file = __dirname + '/out/'+time.getFullYear()+'-'+time.getMonth()+'-'+time.getDay()+'-'+time.getHours()+'.geojson'
-    if(file !== currentFile) {
-      buses = {}
-      currentFile = file
-    }
+      var file = __dirname + '/out/'+time.getFullYear()+'-'+time.getMonth()+'-'+time.getDay()+'-'+time.getHours()+'.geojson'
+      if(file !== currentFile) {
+        buses = {}
+        currentFile = file
+      }
 
-    data.forEach(function(bus){
-      if(!buses[bus.id]) {
-        buses[bus.id] = {
-          route_id: bus.route_id,
-          run_id: bus.run_id,
-          route: turf.linestring([], {route: bus.route_id, run: bus.run_id, times: []}),
-          locations: turf.featurecollection([])
+      data.forEach(function(bus){
+        if(!buses[bus.id]) {
+          buses[bus.id] = {
+            route_id: bus.route_id,
+            run_id: bus.run_id,
+            route: turf.linestring([], {route: bus.route_id, run: bus.run_id, times: []}),
+            locations: turf.featurecollection([])
+          }
         }
-      }
 
-      var lastCoord = buses[bus.id].route.geometry.coordinates[buses[bus.id].route.geometry.coordinates.length - 1]
-      if(buses[bus.id].route.geometry.coordinates.length === 0 || !(lastCoord[0] === bus.longitude && lastCoord[1] === bus.latitude)){
-        buses[bus.id].route.geometry.coordinates.push([
-            bus.longitude,
-            bus.latitude
-          ])
-        buses[bus.id].route.properties.times.push(time.getMinutes()+':'+time.getSeconds())
-      }
-    })
-    fs.writeFileSync(file, JSON.stringify(getTraces(buses)))
+        var lastCoord = buses[bus.id].route.geometry.coordinates[buses[bus.id].route.geometry.coordinates.length - 1]
+        if(buses[bus.id].route.geometry.coordinates.length === 0 || !(lastCoord[0] === bus.longitude && lastCoord[1] === bus.latitude)){
+          buses[bus.id].route.geometry.coordinates.push([
+              bus.longitude,
+              bus.latitude
+            ])
+          buses[bus.id].route.properties.times.push(time.getMinutes()+':'+time.getSeconds())
+        }
+      })
+      fs.writeFileSync(file, JSON.stringify(getTraces(buses)))
+    } catch(e){
+      console.log(e)
+    }
   })
 }, 10000)
 
